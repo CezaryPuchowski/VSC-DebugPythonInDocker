c8b7e62306fdea565eefd34e65d9c86a3ec042a4
CezaryPuchowksi <puchowski.cezary@gmail.com>
2023-07-10 13:47:50 +0200

    Debugging Python in docker container

---
+++ myapp.py
@@ -0,0 +1,25 @@
+# Fibonacci numbers module
+
+import sys
+
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):   # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+def main():
+    fib(15)
+    print(fib2(15))
+
+if __name__ == '__main__':
+    sys.exit(main())
